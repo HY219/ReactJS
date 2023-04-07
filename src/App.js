be898bcadbb94eb4426dc8f687285277c04a886f
hy219 <jhy_0219@daum.net>
2023-04-07 17:04:13 +0900

    컴포넌트에 태그의 속성 전달하기_input에 minLength를 default로 할당(추가)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const Father = styled.div`
 `;
 
 //Input으로 전달될 모든 속성을 가진 오브젝트를 담을 수 있다.
-const Input = styled.input.attrs({ required: true })`
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
   background-color: tomato;
 `;
 
