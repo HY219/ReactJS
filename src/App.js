6be4e2e142fed95766603ee7eda22b4ce292b364
hy219 <jhy_0219@daum.net>
2023-04-07 17:21:31 +0900

    styled-component animation적용 후

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+`;
+
+//animation 사용
+//keyframes string을 import 해준다.
+const rotationAnimation = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
 `;
 
 const Box = styled.div`
   height: 200px;
   width: 200px;
   background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
 `;
+//linear : 애니메이션 효과가 처음부터 끝까지 일정한 속도로 진행된다.
+//infinite : 애니메이션 효과가 무한히 반복된다.
 
 function App() {
   return (
