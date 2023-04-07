c16859f64f4b5d052b681cc54c8da2254067bb3a
hy219 <jhy_0219@daum.net>
2023-04-07 17:15:05 +0900

    styled-component animation적용 전

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import styled from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-//Input으로 전달될 모든 속성을 가진 오브젝트를 담을 수 있다.
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box></Box>
+    </Wrapper>
   );
 }
 
